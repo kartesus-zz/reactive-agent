72adcb5c736e10eb23dcc46731668afca496197c
Alex Gravem <gravem@gmail.com>
2014-12-31 01:52:15 +0100

    Log implementation

---
+++ test/log_test.js
@@ -0,0 +1,25 @@
+var assert = require('chai').assert;
+
+var Log = require('../lib/log');
+
+suite('Log');
+
+var payload = { some: 'data' };
+var subject = Log.make('INFO', 'Something', payload);
+
+test('is a valid packet', function(){
+  assert.include(subject._keys, 'cid');
+  assert.include(subject._keys, 'uuid');
+  assert.include(subject._keys, 'timestamp');
+  assert.include(subject._keys, 'payload');
+});
+
+test('is a curryied function', function() {
+  var s1 = Log.make('INFO');
+  var s2 = Log.make('INFO','Something', null);
+  var s3 = s1('Something',payload);
+
+  assert.isFunction(s1);
+  assert.isObject(s2);
+  assert.isObject(s3);
+});

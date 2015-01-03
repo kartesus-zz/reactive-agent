fc8b811cd7a1dce2d4f2a305656fcf38d2da78fc
Alex Gravem <gravem@gmail.com>
2015-01-03 19:34:09 +0100

    Fix npm package name

--- test/agent_integration.js
+++ test/agent_integration.js
@@ -1,4 +1,5 @@
 var Agent = require('../index')
+
 var a1 = new Agent('a1',{rabbitmq:'amqp://guest:guest@localhost:5672/hayek'})
 var a2 = new Agent('a2',{rabbitmq:'amqp://guest:guest@localhost:5672/hayek'})
 
@@ -6,14 +7,9 @@ var s = a2.signal('sent', {some: 'data'})
 var d = a2.demand('something', {more: 'data'})
 var l = a2.info('event', {event: 'data'})
 
-a1.input({signal: 'sent'}).on('data', function(packet){
-})
-
-a1.input({demand: 'something'}).on('data', function(packet){
-})
-
-a1.input({log: 'event'}).on('data', function(packet){
-})
+a1.input({signal: 'sent'}).on('data', function(packet){})
+a1.input({demand: 'something'}).on('data', function(packet){})
+a1.input({log: 'event'}).on('data', function(packet){})
 
 setTimeout(function(){
   var send = a2.output()

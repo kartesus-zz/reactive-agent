3d5e6fe3990cfcae44f48b462ef71784310f7389
Alex Gravem <gravem@gmail.com>
2015-01-03 03:59:52 +0100

    Adds logging to agent

--- index.js
+++ index.js
@@ -1,14 +1,98 @@
-var Signal = require('./signal');
-var Demand = require('./demand');
-var Log    = require('./log');
+var where = require('lodash.where')
 
-function Agent(){}
+var Signal = require('./lib/signal')
+var Demand = require('./lib/demand')
+var Log    = require('./lib/log')
 
-Agent.prototype.signal = Signal.make;
-Agent.prototype.demand = Demand.make;
-Agent.prototype.info   = Log.make('INFO');
-Agent.prototype.warn   = Log.make('WARN');
-Agent.prototype.error  = Log.make('ERROR');
-Agent.prototype.fatal  = Log.make('FATAL');
+var stats   = require('./lib/stats')
+
+var servicebus = require('servicebus')
+
+var EventEmitter = require('events').EventEmitter
+
+function Agent(name, config) {
+  this.name = name
+  this.bus  = servicebus.bus()
+
+  this.signal = Signal.make(name)
+  this.demand = Demand.make(name)
+
+  this.info  = Log.make(this.name, 'INFO')
+  this.warn  = Log.make(this.name, 'WARN')
+  this.error = Log.make(this.name, 'ERROR')
+
+  var o = this.output()
+  o(this.info('Agent.Started', config))
+
+  var agent = this
+  process.on('SIGINT', function() { process.exit(0) })
+
+  process.on('exit', function(code) {
+    o(agent.error('Agent.Shutdown', {exitCode: code}))
+  })
+
+  process.on('uncaughtException', function(e) {
+    o(agent.error('Agent.Died', { exception: e.constructor.name,
+                                  message: e.message }))
+    process.exit(1)
+  })
+
+}
+
+Agent.prototype.log = function(packet, inwards) {
+  var colors = require('colors/safe')
+  var c = colors.cyan
+  if( packet.level === 'ERROR') c = colors.red
+  if( packet.level === 'WARN') c = colors.yellow
+  console.log("\n%s " + c("[%s %s %s]") + "\ncid=\"%s\"\n%s",
+    packet.timestamp,
+    this.name,
+    inwards ? '<-' : '->',
+    packet.log || packet.demand || packet.signal,
+    packet.cid,
+    formatLogPayload(packet.payload))
+}
+
+Agent.prototype.output = function() {
+  var send = this.bus.publish.bind(this.bus)
+  var log  = this.log.bind(this)
+  return function(packet) {
+    var obj = packet.toJS()
+    send(obj.demand || obj.signal || obj.log, obj)
+    log(packet)
+  }
+}
+
+Agent.prototype.input = function(spec) {
+  var emitter = new EventEmitter()
+  var log     = this.log.bind(this)
+  this.bus.subscribe(spec.demand || spec.signal || spec.log, function(msg) {
+    var match = where([msg], spec)
+    if( match.length === 0 ) return
+
+    var packet;
+    if( msg.demand )      packet = Demand.new(msg)
+    else if( msg.signal ) packet = Signal.new(msg)
+    else if( msg.log )    packet = Log.new(msg)
+
+    emitter.emit('data', packet)
+    log(packet, true)
+  })
+
+  return emitter;
+}
 
 module.exports = Agent;
+
+function formatLogPayload(payload) {
+  return Object.keys(payload)
+               .map(function(k) { return k + '="' + format(payload[k]) + '"' })
+               .join('\n')
+}
+
+function format(value) {
+  var inspect = require('util').inspect
+  if( value.constructor.name === 'String' ) return value
+  if( value.constructor.name === 'Number' ) return value
+  return inspect(value)
+}
---
+++ test/agent_integration.js
@@ -0,0 +1,23 @@
+var Agent = require('../index')
+var a1 = new Agent('a1',{rabbitmq:'amqp://guest:guest@localhost:5672/hayek'})
+var a2 = new Agent('a2',{rabbitmq:'amqp://guest:guest@localhost:5672/hayek'})
+
+var s = a2.signal('sent', {some: 'data'})
+var d = a2.demand('something', {more: 'data'})
+var l = a2.info('event', {event: 'data'})
+
+a1.input({signal: 'sent'}).on('data', function(packet){
+})
+
+a1.input({demand: 'something'}).on('data', function(packet){
+})
+
+a1.input({log: 'event'}).on('data', function(packet){
+})
+
+setTimeout(function(){
+  var send = a2.output()
+  send(s)
+  send(d)
+  send(l)
+}, 300)

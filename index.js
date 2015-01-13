90c808a59c035071d480f33dbc7ebbce3d43f8e3
Gert Blom <gert@freshtag.nl>
2015-01-13 12:22:35 +0100

    try to use amqp
    Signed-off-by: Alex Gravem <gravem@gmail.com>

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ var Log    = require('./lib/log')
 
 var Logger = require('./lib/logger');
 
-var servicebus = require('servicebus')
+var amqp = require('amqp')
 
 var EventEmitter = require('events').EventEmitter
 
 function Agent(name, config) {
-  this.name = name
-  this.bus  = servicebus.bus()
+  this.name = name;
 
   this.signal = Signal.make(name)
   this.demand = Demand.make(name)
@@ -23,56 +22,93 @@ function Agent(name, config) {
 
   Agent.Logger = Agent.Logger || Logger;
 
-  var o = this.output()
-  o(this.info('Agent.Started', config))
+  var
+      eventEmitter = new EventEmitter(),
+      agent = this;
 
-  var agent = this
-  process.on('SIGINT', function() { process.exit(0) })
+    console.log('Agent');
 
-  process.on('exit', function(code) {
-    o(agent.error('Agent.Shutdown', {exitCode: code}))
-  })
+  var connection = amqp.createConnection(config.rabbitmq, {})
+      .on('error', function(error) {
+          console.log(error);
+      })
+      .on('ready', function() {
 
-  process.on('uncaughtException', function(e) {
-    o(agent.error('Agent.Died', { exception: e.constructor.name,
-                                  message: e.message }))
-    process.exit(1)
-  })
+      connection.queue((config.rabbitmq.queue || 'agent-queue'), function(queue) {
 
-}
+          try{
 
-Agent.prototype.output = function() {
-  var send = this.bus.publish.bind(this.bus)
-  var log  = Agent.Logger.log
-  var agent = this.name;
-  return function() {
-    for(var i in arguments) {
-      var obj = arguments[i].toJS()
-      send(obj.demand || obj.signal || obj.log, obj)
-      log(agent, obj)
-    }
-  }
-}
+              console.log('queue');
 
-Agent.prototype.input = function(spec) {
-  var emitter = new EventEmitter()
-  var log     = Agent.Logger.log;
-  var agent   = this.name;
-  this.bus.subscribe(spec.demand || spec.signal || spec.log, function(msg) {
-    var match = where([msg], spec)
-    if( match.length === 0 ) return
+              queue.bind('#');
 
-    var packet;
-    if( msg.demand )      packet = Demand.new(msg)
-    else if( msg.signal ) packet = Signal.new(msg)
-    else if( msg.log )    packet = Log.new(msg)
+              Agent.prototype.output = function() {
+                  var send = connection.publish;
+                  var log  = Agent.Logger.log
+                  var agent = this.name;
 
-    emitter.emit('data', packet)
-    log(agent, packet)
-  })
+                  return function() {
+                      for(var i in arguments) {
+                          var obj = arguments[i].toJS()
+                          send(
+                              (obj.demand || obj.signal || obj.log),
+                              obj
+                          );
+                          log(agent, obj)
+                      }
+                  }
+              };
 
-  return emitter;
-}
+              Agent.prototype.input = function(spec) {
+                  var emitter = new EventEmitter();
+                  var log     = Agent.Logger.log;
+                  var agent   = this.name;
 
+                  queue.subscribe(spec.demand || spec.signal || spec.log, function(msg) {
+                      var match = where([msg], spec);
+                      if (match.length === 0) {
+                          return;
+                      }
+
+                      var packet;
+                      if ( msg.demand )      packet = Demand.new(msg)
+                      else if ( msg.signal ) packet = Signal.new(msg)
+                      else if ( msg.log )    packet = Log.new(msg)
+
+                      emitter.emit('data', packet)
+                      log(agent, packet)
+                  });
+
+                  return emitter;
+              };
+
+              var o = agent.output()
+
+              o(agent.info('Agent.Started', config))
+              process.on('SIGINT', function() { process.exit(0) })
+
+              process.on('exit', function(code) {
+                  o(agent.error('Agent.Shutdown', {exitCode: code}))
+              })
+
+              process.on('uncaughtException', function(e) {
+                  o(agent.error('Agent.Died', { exception: e.constructor.name,
+                      message: e.message }))
+                  process.exit(1)
+              });
+
+              console.log('emit ready');
+
+              eventEmitter.emit('ready', agent);
+
+          }
+          catch (e) {
+              console.log(e);
+          }
+      });
+  });
+
+  return eventEmitter;
+};
 module.exports = Agent;
 
